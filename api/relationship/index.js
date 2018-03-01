00c3b79e395594c5a0bad245d6cf678d6d0ba492
Francisco Farias <fariasfranciscoe@gmail.com>
2018-03-01 02:43:49 -0300

    renamed endpoint

---
+++ api/relationship/index.js
@@ -0,0 +1,12 @@
+'use strict'
+
+import { Router } from 'express'
+import * as relationship from './relationship.controller'
+import * as middleware from '../../components/middleware/index'
+
+const router = new Router()
+
+router.get('/children', middleware.logRequest('relationship', relationship.getChildren.name), relationship.getChildren)
+router.get('/parent', middleware.logRequest('relationship', relationship.getParents.name), relationship.getParents)
+
+module.exports = router
