51f12255fa6b079b7e49a1876994846d52df349a
Francisco Farias <fariasfranciscoe@gmail.com>
2018-03-08 16:17:09 -0300

    minor fixes

--- components/errors/index.js
+++ components/errors/index.js
@@ -4,7 +4,7 @@ class BaseError extends Error {
   constructor (parent) {
     super(parent.message)
 
-    this.name = 'SnomedBaseError'
+    this.name = 'SNOMEDBaseError'
     this.requestId = parent.requestId
     this.statusCode = 418
 
@@ -19,7 +19,7 @@ class APIError extends BaseError {
     this.method = parent.method
     this.endpoint = parent.endpoint
     this.controllerFunction = parent.controllerFunction
-    this.name = 'SnomedAPIError'
+    this.name = 'SNOMEDAPIError'
     this.statusCode = 400
 
     Error.captureStackTrace(this, this.constructor)
@@ -31,7 +31,7 @@ class APIParamMissingError extends APIError {
     super(parent)
 
     this.missingParams = parent.missingParams || []
-    this.name = 'SnomedAPIParamMissingError'
+    this.name = 'SNOMEDAPIParamMissingError'
 
     Error.captureStackTrace(this, this.constructor)
   }
@@ -42,7 +42,7 @@ class APIParamInvalidError extends APIError {
     super(parent)
 
     this.invalidParams = parent.invalidParams || []
-    this.name = 'SnomedAPIParamInvalidError'
+    this.name = 'SNOMEDAPIParamInvalidError'
 
     Error.captureStackTrace(this, this.constructor)
   }
@@ -53,7 +53,7 @@ class FeatureUnavailableError extends BaseError {
     super(parent)
 
     this.feature = parent.feature
-    this.name = 'SnomedFeatureUnavailableError'
+    this.name = 'SNOMEDFeatureUnavailableError'
     this.statusCode = 503
 
     Error.captureStackTrace(this, this.constructor)
@@ -64,7 +64,7 @@ class FeatureFileError extends BaseError {
   constructor (parent) {
     super(parent)
 
-    this.name = 'SnomedFeatureFileError'
+    this.name = 'SNOMEDFeatureFileError'
 
     Error.captureStackTrace(this, this.constructor)
   }
@@ -74,7 +74,7 @@ class FeatureFileMissingError extends FeatureFileError {
   constructor (parent) {
     super(parent)
 
-    this.name = 'SnomedFeatureFileMissingError'
+    this.name = 'SNOMEDFeatureFileMissingError'
     this.statusCode = 404
 
     Error.captureStackTrace(this, this.constructor)
@@ -85,7 +85,7 @@ class InvalidFeatureNameError extends FeatureFileError {
   constructor (parent) {
     super(parent)
 
-    this.name = 'SnomedInvalidFeatureNameError'
+    this.name = 'SNOMEDInvalidFeatureNameError'
     this.featureName = parent.featureName
 
     Error.captureStackTrace(this, this.constructor)
