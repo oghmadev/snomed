283a7692c4a5e4d00c70026df314b819efbea7de
Francisco Farias <fariasfranciscoe@gmail.com>
2018-03-21 16:28:20 -0300

    asd

--- app/config/express.js
+++ app/config/express.js
@@ -11,16 +11,37 @@ import logger from '../config/logs'
 
 const morganFormat = ':date[iso],:req[X-Request-ID],:method,:url,:status,:response-time,:res[content-length],:referrer,:remote-user,:remote-addr,:req[header],:user-agent'
 
+function morganJSONFormat (message) {
+  const morganParts = message.split(',')
+  const partNames = ['timestamp', 'requestId', 'method', 'url', 'status', 'responseTime', 'contentLength', 'referrer', 'remoteUser', 'remoteAddress', 'requestHeader']
+  const logObject = {}
+
+  for (let [i, name] of partNames.entries()) {
+    logObject[name] = morganParts[i]
+  }
+
+  logObject.userAgent = morganParts.slice(partNames.length).join(',')
+
+  return logObject
+}
+
+function logExpress (level) {
+  return message => {
+    logger.express.console[level](message.trim())
+    logger.express.file.log(Object.assign(morganJSONFormat(message.trim()), {level}))
+  }
+}
+
 export default function (app) {
   const env = app.get('env')
 
   app.use(morgan(morganFormat, {
     skip: (req, res) => res.statusCode >= 400,
-    stream: {write: message => logger.express.info(message.trim())}
+    stream: {write: logExpress('info')}
   }))
   app.use(morgan(morganFormat, {
     skip: (req, res) => res.statusCode < 400,
-    stream: {write: message => logger.express.error(message.trim())}
+    stream: {write: logExpress('error')}
   }))
   app.set('views', config.root + '/server/views')
   app.engine('html', require('ejs').renderFile)
